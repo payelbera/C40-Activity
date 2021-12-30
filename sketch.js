115ed95d9b72b8acc5b17513d0ae5141ae72a0e2
payelbera <60781680+payelbera@users.noreply.github.com>
2021-12-30 15:54:39 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,6 @@ var canvas;
 var backgroundImage, car1_img, car2_img, track;
 var fuelImage, powerCoinImage;
 var obstacle1Image, obstacle2Image;
-
 var database, gameState;
 var form, player, playerCount;
 var allPlayers, car1, car2, fuels, powerCoins, obstacles;
@@ -15,8 +14,8 @@ function preload() {
   track = loadImage("../assets/track.jpg");
   fuelImage = loadImage("./assets/fuel.png");
   powerCoinImage = loadImage("./assets/goldCoin.png");
-  obstacle1Image = loadImage("./assets/obstacle1.png")
-  obstacle2Image = loadImage("./assets/obstacle2.png")
+  obstacle1Image = loadImage("./assets/obstacle1.png");
+  obstacle2Image = loadImage("./assets/obstacle2.png");
 }
 
 function setup() {
